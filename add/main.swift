1e34cd3cc325390094c57d4509cf35f8b10ad3a7
lijiarun <1103385493@qq.com>
2018-09-29 10:16:20 +0800

    Initial commit

---
+++ add/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  add
+//
+//  Created by s20171103189 on 2018/9/29.
+//  Copyright © 2018年 s20171103189. All rights reserved.
+//
+
+import Foundation
+
+var sum = 0
+
+for i in 1...99
+{
+    sum+=i;
+}
+print(sum)
